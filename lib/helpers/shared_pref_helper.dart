f31fabf2c256b2d82914b39080715575a00ce5b1
Eashubh Thapliyal <eashubhthapliyal@eashubhs-macbook-pro.local>
2026-02-02 01:17:06 +0530

    Added goal achievement functionality

--- lib/helpers/shared_pref_helper.dart
+++ lib/helpers/shared_pref_helper.dart
@@ -8,6 +8,7 @@ class SharedPrefsHelper {
   static const String _keyPersonalInfoSubmitted = 'personal_info_submitted';
   static const String _keyLastConnectedBleName = 'last_connected_ble_name';
   static const String _keyLastConnectedBleId = 'last_connected_ble_id';
+  static const String _keyLastLevelUpDate = 'last_level_up_date';
 
   static const String _bottleKey = 'selected_bottle';
 
@@ -75,8 +76,7 @@ class SharedPrefsHelper {
 
   static Future<String?> getUserEmail() async {
     final prefs = await SharedPreferences.getInstance();
-    // return prefs.getString(_keyUserEmail);
-    return "normal_user";
+    return prefs.getString(_keyUserEmail);
   }
 
   static Future<int?> getUserGoal() async {
@@ -191,93 +191,14 @@ class SharedPrefsHelper {
 
 // import 'package:shared_preferences/shared_preferences.dart';
 
-// class SharedPrefsHelper {
-//   // Keys
-//   static const String _keyUserEmail = 'user_email';
-//   static const String _keyWaterGoal = 'water_goal';
-//   static const _keyUserGoal = 'user_goal';
-//   static const String _keyPersonalInfoSubmitted = 'personal_info_submitted';
-//   static const String _keyLastConnectedBleName = 'last_connected_ble_name';
-//   static const String _keyLastConnectedBleId = 'last_connected_ble_id';
+  static Future<void> setLastLevelUpDate(String date) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_keyLastLevelUpDate, date);
+  }
 
-//   // Save user email
-//   static Future<void> setUserEmail(String email) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setString(_keyUserEmail, email);
-//   }
-
-//   static Future<void> setWaterGoal(int goal) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setInt(_keyWaterGoal, goal);
-//   }
-
-//   static Future<void> setUserGoal(int goal) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setInt(_keyUserGoal, goal);
-//   }
-
-//   static Future<int?> getWaterGoal() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getInt(_keyWaterGoal);
-//   }
-
-//   static Future<String?> getUserEmail() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getString(_keyUserEmail);
-//   }
-
-//   static Future<int?> getUserGoal() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getInt(_keyWaterGoal);
-//   }
-
-//   static Future<int?> getUserGoals() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getInt(_keyUserGoal);
-//   }
-
-//   // Personal info submitted
-//   static Future<void> setPersonalInfoSubmitted(bool value) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setBool(_keyPersonalInfoSubmitted, value);
-//   }
-
-//   static Future<bool> isPersonalInfoSubmitted() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getBool(_keyPersonalInfoSubmitted) ?? false;
-//   }
-
-//   // BLE info
-//   static Future<void> setLastConnectedBleName(String name) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setString(_keyLastConnectedBleName, name);
-//   }
-
-//   static Future<void> setLastConnectedBleId(String id) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setString(_keyLastConnectedBleId, id);
-//   }
-
-//   static Future<String?> getLastConnectedBleName() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getString(_keyLastConnectedBleName);
-//   }
-
-//   static Future<String?> getLastConnectedBleId() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getString(_keyLastConnectedBleId);
-//   }
-
-//   // Clear all stored values
-//   static Future<void> clearAll() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.clear();
-//   }
-
-//   static Future<void> clearLastConnectedDeviceData() async {
-//     final prefs = await SharedPreferences.getInstance();
-
-//     await prefs.remove(_keyLastConnectedBleName);
-//     await prefs.remove(_keyLastConnectedBleId);
-//   }
-// }
+  /// Retrieves the date string of the last level up to prevent duplicate notifications.
+  static Future<String?> getLastLevelUpDate() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_keyLastLevelUpDate);
+  }
+}
