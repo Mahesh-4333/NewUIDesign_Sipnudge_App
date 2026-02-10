b4bcd24b0395e3a6884cf31a6d30dcbc8a085f88
vipcoderror123 <151127652+vipcoderror123@users.noreply.github.com>
2026-02-11 02:29:53 +0530

    intake assets icon

---
+++ lib/constants/assets_path.dart
@@ -0,0 +1,13 @@
+class AssetsPath {
+  static const String intakeTimeline = "assets/intake_timeline/";
+
+  static const String wakeUp = "${intakeTimeline}wake.png";
+  static const String breakfast = "${intakeTimeline}breakfast.png";
+  static const String lunch = "${intakeTimeline}lunch.png";
+  static const String dinner = "${intakeTimeline}dinner.png";
+  static const String evening = "${intakeTimeline}evening.png";
+  static const String midAfternoon = "${intakeTimeline}mid_afternoon.png";
+  static const String midMorning = "${intakeTimeline}mid_morning.png";
+
+
+}
