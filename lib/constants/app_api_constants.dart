3b640db36737e434acf3f730bca3f436597ab04c
vipcoderror123 <151127652+vipcoderror123@users.noreply.github.com>
2026-04-22 13:12:28 +0530

    Update app_api_constants.dart
    api key removed

--- lib/constants/app_api_constants.dart
+++ lib/constants/app_api_constants.dart
@@ -2,5 +2,5 @@ class AppApiConstants {
   static final success = "success";
 
   static final baseUrl = "https://sipnudge-production.up.railway.app/api/v1";
-  static const geminiApiKey = "AQ.Ab8RN6Lwx429wZcrb46TYC5L6J3CPcXLv3nWM3PPPUbGYzqVtw";
+
 }
