fd14f25cc7861a1f98cac4824248fdc4b4f99a91
vipcoderror123 <151127652+vipcoderror123@users.noreply.github.com>
2026-04-28 18:38:19 +0530

    commit

--- lib/constants/app_api_constants.dart
+++ lib/constants/app_api_constants.dart
@@ -2,5 +2,5 @@ class AppApiConstants {
   static final success = "success";
 
   static final baseUrl = "https://sipnudge-production.up.railway.app/api/v1";
-  static const geminiApiKey = "AIzaSyDbIoZkXEeUnYmjrYXD6U27dC38F3yQru8";
+
 }
